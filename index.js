3df8e5d94bd6ae649e0d7bb85a627ddf9fe7a1ce
Seyit Enes <seyit7enes@gmail.com>
2023-03-18 08:01:45 +0100

    change to aysnc

--- index.js
+++ index.js
@@ -2,23 +2,22 @@
 const image = document.querySelector("img");
 const input = document.querySelector("input");
 const loadingAnimation = document.querySelector(".wrapper");
+const defaultImage =
+  "https://images.unsplash.com/photo-1453227588063-bb302b62f50b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8c2FkJTIwYW5pbWFsfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=70";
 
-function requestApi(search) {
-  fetch("API_KES" + search, { mode: "cors" })
-    .then((response) => {
-      if (!response.ok) throw new Error("API ERROR");
-      return response.json();
-    })
-    .then((responseData) => {
-      image.src = responseData.data.images.original.url;
-    })
-    .catch((error) => {
-      console.log(error);
-
-      image.src =
-        "https://images.unsplash.com/photo-1453227588063-bb302b62f50b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8c2FkJTIwYW5pbWFsfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60";
-    });
+async function requestApi(search) {
+  try {
+    const response = await fetch("API_KES" + search, { mode: "cors" });
+    const data = await response.json();
+    image.src = data.data.imaes.original.url;
+  } catch (error) {
+    console.error(error);
+    image.src = defaultImage;
+  }
 }
+//
+
+//
 input.addEventListener("keydown", (event) => {
   if (event.code === "Enter") {
     loadingAnimation.classList.remove("hide");
